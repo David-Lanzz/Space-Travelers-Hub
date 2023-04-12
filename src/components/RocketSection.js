65fbe542831115792217000fa5f53f086c426552
Olanrewaju Oduola <111298026+David-Lanzz@users.noreply.github.com>
2023-04-12 12:34:23 +0100

    Fix conflict manually

--- src/components/RocketSection.js
+++ src/components/RocketSection.js
@@ -1,23 +1,28 @@
-import { useSelector } from 'react-redux';
-import '../styles/rocket.css';
-
+import { useSelector } from "react-redux";
+import {changestate} from '../redux/Rockets/rocketSlice'
+import '../styles/rocket.css'
 const RocketSection = () => {
+  const handleChange = (id)=> {
+    dispatch(changestate(id))
+      }
   const store = useSelector((store) => store.rocketReducer);
   const { rockets, loading } = store;
   if (loading) {
     return <h2>Loading...</h2>;
   }
-  return rockets.map((rocket) => (
-    <li className="rocketgrid" key={rocket.id}>
-      {' '}
-      <img src={rocket.flickr_images} alt={rocket.name} className="rocketimage" />
-      <div className="rocketdata">
+  console.log(rockets);
+  return rockets.map((rocket) => {
+    return (
+      <li className="rocketgrid" key={rocket.id}>
+        {" "}
+        <img src={rocket.flickr_images} alt={rocket.name} className="rocketimage" />
+        <div className="rocketdata">
         <h2 className="rockettitle">{rocket.name}</h2>
-        <p>{rocket.description}</p>
-        <button type="button"> Reserve Rocket</button>
-      </div>
-    </li>
-  ));
+        <p>{rocket.reserved ? 'Reserved': ''} {rocket.description}</p>
+        <button type="button" onClick={()=> handleChange(rocket.id)}>{rocket.reserved?'Cancel Reservation':'Reserve a rocket'}</button></div>
+      </li>
+    );
+  });
 };
 
 export default RocketSection;
