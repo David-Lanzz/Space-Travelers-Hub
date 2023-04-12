fc26686b122d326c9bdc3bfe42dfb9ed3fcb39c0
Walid Kabboussa <walidkabou@gmail.com>
2023-04-12 17:58:28 +0000

    Style website pages and buttons

--- src/components/missionsection.js
+++ src/components/missionsection.js
@@ -17,8 +17,25 @@ const Missionsection = () => {
       <li className="grid tablerow" style={{ background: mission.background }} key={mission.mission_id}>
         <h3 className="rightborder">{mission.mission_name}</h3>
         <p className="rightborder">{mission.description}</p>
-        <span className="rightborder center"><button type="button">{mission.member ? 'Active Member' : 'NOT A MEMBER'}</button></span>
-        <span className="rightborder center"><button type="button" onClick={() => handleChange(mission.mission_id)}>{mission.member ? 'Leave Mission' : 'Join Mission'}</button></span>
+        <span className="rightborder center">
+          <button
+            className={mission.member ? 'active' : ''}
+            id="activemember"
+            type="button"
+          >
+            {mission.member ? 'Active Member' : 'NOT A MEMBER'}
+          </button>
+        </span>
+        <span className="rightborder center">
+          <button
+            className={mission.member ? 'active' : ''}
+            id="joinmission"
+            type="button"
+            onClick={() => handleChange(mission.mission_id)}
+          >
+            {mission.member ? 'Leave Mission' : 'Join Mission'}
+          </button>
+        </span>
       </li>
     ))
   );
